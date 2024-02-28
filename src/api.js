04c54a5893992e185a8c8628000755be5a2af97b
Bryan F. Rocha <bryanrocha@Bryans-Air.attlocal.net>
2024-02-28 13:27:02 -0600

    second commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "41c591cd27msh042d500d14ff0c1p188da8jsnaf0552cbdb39",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "aead2259c87e48b5e0c42d3c461f5783"
+
+
